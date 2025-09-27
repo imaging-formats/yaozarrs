2dfa3c0057ddb9567e4b39c5b34bdb738c578dcd
Talley Lambert <talley.lambert@gmail.com>
2025-09-27 13:32:38 -0400

    wip

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,27 +1,29 @@
-import json
 from pathlib import Path
 
 import pytest
 
-from yaozarrs import OMEZarr
+from yaozarrs.v05._zarr_json import ZarrJSON
 
 DATA = Path(__file__).parent / "data"
-ZARR_JSONS = sorted(DATA.rglob("zarr.json"))
-OME_ATTRS: dict[str, dict] = {
-    str(path.parent.relative_to(DATA)): data["attributes"]
+# ALL of the zarr.json files in the test data
+ZARR_JSONS = sorted(x for x in DATA.rglob("zarr.json"))
+# The *contents* of all zarr.json files that contain OME metadata
+OME_ZARR_JSONS: dict[str, str] = {
+    str(path.relative_to(DATA)): content
     for path in ZARR_JSONS
-    if isinstance(data := json.loads(path.read_text()), dict)
-    and "ome" in data.get("attributes", {})
+    if '"ome"' in (content := path.read_text())
 }
 
 
-@pytest.mark.parametrize("attrs", OME_ATTRS.values(), ids=OME_ATTRS.keys())
-def test_data(attrs: dict) -> None:
-    obj = OMEZarr.model_validate(attrs)
+@pytest.mark.parametrize(
+    "txt", OME_ZARR_JSONS.values(), ids=list(OME_ZARR_JSONS.keys())
+)
+def test_data(txt: str) -> None:
+    obj = ZarrJSON.model_validate_json(txt)
 
     # FIXME:
     # we shouldn't have to do by_alias=True here...
     # but it's required for pydantic <2.10.0
     js = obj.model_dump_json(by_alias=True)
-    obj2 = OMEZarr.model_validate_json(js)
+    obj2 = ZarrJSON.model_validate_json(js)
     assert obj == obj2
